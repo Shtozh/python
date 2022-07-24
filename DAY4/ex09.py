016efadb1cc7fac1b92b9fb3421bccb345e9c0b2
Shtozh <101527417+Shtozh@users.noreply.github.com>
2022-07-24 18:00:06 +0300

    Update ex09.py

--- DAY4/ex09.py
+++ DAY4/ex09.py
@@ -1,10 +1,12 @@
-x = float(input())
-if x <= -2:
-    y = 1 - (x+2)*(x+2)
-    print(y)
-elif -2 < x <= 2:
-    y = -x/2
-    print(y)
-elif 2 < x:
-    y = (x-2)*(x-2) + 1
-    print(y)
+def func1(x):
+    if x <= -2:
+        y = 1 - (x+2)*(x+2)
+        print(y)
+    elif -2 < x <= 2:
+        y = -x/2
+        print(y)
+    elif 2 < x:
+        y = (x-2)*(x-2) + 1
+        print(y)
+x = 1
+func1(x)
